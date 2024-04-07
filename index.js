838febdfcf295f45534b13e807be1c3b2e2b1377
Joel John <johnjoel079@gmail.com>
2024-04-07 16:37:41 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution in this file!
+// Code your solution in this file! 
+const returnFirstTwoDrivers = function (drivers) { 
+ return drivers.slice(0,2)
+}
+
+const returnLastTwoDrivers = function (drivers) { 
+    return drivers.slice(-2)
+} 
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]; 
+
+function createFareMultiplier(multiplier) { 
+    return function(value) { 
+        return value * multiplier 
+    }
+} 
+
+const fareDoubler = createFareMultiplier(2) 
+console.log(fareDoubler(10)) 
+
+const fareTripler = createFareMultiplier(3) 
+console.log (fareTripler(10))
+
+function selectDifferentDrivers(drivers,func) { 
+    return func(drivers) 
+} 
+ returnFirstTwoDrivers(drivers);  
+ returnLastTwoDrivers(drivers); 
+
+
+
+
+
+
